93d18596907e4644f23eebcafef8ee66ae7e0880
Parth0-5 <124764271+Parth0-5@users.noreply.github.com>
2024-01-24 12:08:06 +0530

    Update test.js

--- 1_basic/test.js
+++ 1_basic/test.js
@@ -1,1 +1,8 @@
-console.log("Parth")
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassowrd = "12345"
+accountCity = "Jiapur"
+
+accountId = 2 
+
+console.log(accountId);
